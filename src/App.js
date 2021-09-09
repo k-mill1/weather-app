e60bde35b0a3ba300e8af00add1976c483f05126
k-mill1 <kaisamill@gmail.com>
2021-09-09 16:25:27 +0100

    add hourly weather

--- src/App.js
+++ src/App.js
@@ -13,17 +13,14 @@ function App() {
   const [dailyWeather, cDailyWeather] = useState({
     weather: []
   })
-  // const [currentWeather, cCurrentWeather] = useState({
-  //   weather: [{temp: 0, icon : ""}]
-  // })
-
+ 
   const apiClient = new ApiClient();
 
   const updateDailyWeather = (response) => {
     cDailyWeather({weather : response})
   }
-
-  const destructureWeather = (obj, currentObj)=> {
+ 
+  const destructureWeather = (obj, currentObj, hourlyObj)=> {
     cDay({
       dateString: obj.dt,     
       img: obj.weather[0].icon, 
@@ -34,47 +31,36 @@ function App() {
       wind_speed: obj.wind_speed,
       pressure: obj.pressure,
       humidity: obj.humidity,
-      currentWeather: currentObj.temp
-    })
-  }
-
-
-  // const updateCurrentWeather = (response) => {
-  //   cCurrentWeather({
-  //     weather: response
-  //   })
-  // }
+      currentWeather: currentObj.temp,
+      hourOneDt: hourlyObj[1].dt,
+      hourOneTemp: hourlyObj[1].temp,
+      hourOneIcon: hourlyObj[1].weather[0].icon,
+      hourTwoDt: hourlyObj[2].dt,
+      hourTwoTemp: hourlyObj[2].temp,
+      hourTwoIcon: hourlyObj[2].weather[0].icon,
+      hourThreeDt: hourlyObj[3].dt,
+      hourThreeTemp: hourlyObj[3].temp,
+      hourThreeIcon: hourlyObj[3].weather[0].icon,
+      hourFourDt: hourlyObj[4].dt,
+      hourFourTemp: hourlyObj[4].temp,
+      hourFourIcon: hourlyObj[4].weather[0].icon,
+      hourFiveDt: hourlyObj[5].dt,
+      hourFiveTemp: hourlyObj[5].temp,
+      hourFiveIcon: hourlyObj[5].weather[0].icon,
+      hourSixDt: hourlyObj[6].dt,
+      hourSixTemp: hourlyObj[6].temp,
+      hourSixIcon: hourlyObj[6].weather[0].icon,
+})
+}
+console.log(day)
   const fetchWeather = () => {
     apiClient
       .getWeather()
       .then((res) => {
         updateDailyWeather(res.data.daily)
-        destructureWeather(res.data.daily[0], res.data.current)
-        // cDay(res.data.daily[0])
-        // updateCurrentWeather(res.data.current)
-        // console.log(res.data.daily)
-        
+        destructureWeather(res.data.daily[0], res.data.current, res.data.hourly)
       })
   }
-
-  // const buildFirstCard = () => {
-  //   // let todayArr = dailyWeather.weather.slice(0, 1)
-  //   // let today = todayArr[todayArr.length-1]
-  //   // console.log("today", today)
-  //   // console.log(currentWeather.weather.temp)
-  //   return dailyWeather.weather.slice(0, 1).map((daily, i) => (
-  //     <Row key={i}>
-  //       <CardGroup>
-  //         <ScardFirst currentWeather = {currentWeather.temp} dateString={daily.dt} img={daily.weather[0].icon} alt={daily.weather[0].description} text={daily.weather[0].description} 
-  //         max={daily.temp.max} min={daily.temp.min} wind={daily.wind_speed} pressure={daily.pressure} humidity={daily.humidity}/>
-  //       </CardGroup>
-  //     </Row>
-  //   ))
-  // }
-
-  // state = [weather, cWeather] = [1,2,3,4]
-  //state2 = [weather2, cw2] = {key : undefined}
-  // weather.map(() => console.log(weather2.key[0])) //error
 
   const buildCards = () => {
     return dailyWeather.weather.slice(1, 7).map((current, i) => (
@@ -115,7 +101,26 @@ function App() {
                wind={day.wind_speed}
                humidity={day.humidity}
                pressure={day.pressure}
-              currentWeather ={day.currentWeather}/>
+               currentWeather ={day.currentWeather}
+               hourOneDt= {day.hourOneDt}
+               hourTwoDt= {day.hourTwoDt}
+               hourThreeDt= {day.hourThreeDt}
+               hourFourDt= {day.hourFourDt}
+               hourFiveDt= {day.hourFiveDt}
+               hourSixDt= {day.hourSixDt}
+               hourOneTemp= {day.hourOneTemp}
+               hourTwoTemp= {day.hourTwoTemp}
+               hourThreeTemp= {day.hourThreeTemp}
+               hourFourTemp= {day.hourFourTemp}
+               hourFiveTemp= {day.hourFiveTemp}
+               hourSixTemp= {day.hourSixTemp}
+               hourOneIcon= {day.hourOneIcon}
+               hourTwoIcon= {day.hourTwoIcon}
+               hourThreeIcon= {day.hourThreeIcon}
+               hourFourIcon= {day.hourFourIcon}
+               hourFiveIcon= {day.hourFiveIcon}
+               hourSixIcon= {day.hourSixIcon}
+               />
             </CardGroup>
           </Row>
           {buildCards()}
--- src/scardFirst.js
+++ src/scardFirst.js
@@ -8,6 +8,7 @@ import './App.css';
 function ScardFirst(props) {
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const hours = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"]
   const day = new Date(parseInt(props.dateString) * 1000);
   const nameDay = days[day.getDay()]
   const month = months[day.getMonth()]
@@ -17,6 +18,25 @@ function ScardFirst(props) {
   const minTemp = Math.round(props.min)
   const windSpeed = Math.round(props.wind)
   const currentTemp = Math.round(props.currentWeather)
+  const hourOne = new Date(parseInt(props.hourOneDt) * 1000);
+  const hourOneTime = hours[hourOne.getHours()]
+  const hourTwo = new Date(parseInt(props.hourTwoDt) * 1000);
+  const hourTwoTime = hours[hourTwo.getHours()]
+  const hourThree = new Date(parseInt(props.hourThreeDt) * 1000);
+  const hourThreeTime = hours[hourThree.getHours()]
+  const hourFour = new Date(parseInt(props.hourFourDt) * 1000);
+  const hourFourTime = hours[hourFour.getHours()]
+  const hourFive = new Date(parseInt(props.hourFiveDt) * 1000);
+  const hourFiveTime = hours[hourFive.getHours()]
+  const hourSix = new Date(parseInt(props.hourSixDt) * 1000);
+  const hourSixTime = hours[hourSix.getHours()]
+  const hourOneIcon = `http://openweathermap.org/img/wn/${props.hourOneIcon}@2x.png`
+  const hourTwoIcon = `http://openweathermap.org/img/wn/${props.hourTwoIcon}@2x.png`
+  const hourThreeIcon = `http://openweathermap.org/img/wn/${props.hourThreeIcon}@2x.png`
+  const hourFourIcon = `http://openweathermap.org/img/wn/${props.hourFourIcon}@2x.png`
+  const hourFiveIcon = `http://openweathermap.org/img/wn/${props.hourFiveIcon}@2x.png`
+  const hourSixIcon = `http://openweathermap.org/img/wn/${props.hourSixIcon}@2x.png`
+console.log(props.hourOneIcon)
   return (
     <>
      
@@ -49,6 +69,40 @@ function ScardFirst(props) {
                         </Row>
                     </div>
                 </Col>
+            </Row>
+            <hr className = "white-line" />
+            <Row>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourOneTemp)}{"°"}</Row>
+                    <Row><Image src={hourOneIcon}/></Row>
+                    <Row className = "hourly-time">{hourOneTime}</Row>
+                </Col>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourTwoTemp)}{"°"}</Row>
+                    <Row><Image src={hourTwoIcon}/></Row>
+                    <Row className = "hourly-time">{hourTwoTime}</Row>
+                </Col>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourThreeTemp)}{"°"}</Row>
+                    <Row><Image src={hourThreeIcon}/></Row>
+                    <Row className = "hourly-time">{hourThreeTime}</Row>
+                </Col>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourFourTemp)}{"°"}</Row>
+                    <Row><Image src={hourFourIcon}/></Row>
+                    <Row className = "hourly-time">{hourFourTime}</Row>
+                </Col>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourFiveTemp)}{"°"}</Row>
+                    <Row><Image src={hourFiveIcon}/></Row>
+                    <Row className = "hourly-time">{hourFiveTime}</Row>
+                </Col>
+                <Col>
+                    <Row className = "hourly-temp">{Math.round(props.hourSixTemp)}{"°"}</Row>
+                    <Row><Image src={hourSixIcon}/></Row>
+                    <Row className = "hourly-time">{hourSixTime}</Row>
+                </Col>
+
             </Row>
           </Card.Body>
         </Card>
